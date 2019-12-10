12c5dffced70c79e6138ea360d87d6d0a72b9c94
helder2999 <helder2999@gmail.com>
2019-12-10 14:45:44 -0100

    Add files via upload

---
+++ precache-manifest.2b8f1c11cf6e9866566b6063e86badf6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "bca3a1873ac988faff0817eca96b2d86",
+    "url": "/fonts/MaterialIcons-Regular.bca3a187.woff2"
+  },
+  {
+    "revision": "75bf8630377ab757f8ee",
+    "url": "/css/chunk-vendors.7d918fdd.css"
+  },
+  {
+    "revision": "ba6cad25afe01d394e830f548a7f94df",
+    "url": "/fonts/SourceSansPro-Regular.ba6cad25.ttf"
+  },
+  {
+    "revision": "364f4e07824d9ee0c999597bc8f1607e",
+    "url": "/img/LOGO_4_SEM_FUNDO.364f4e07.png"
+  },
+  {
+    "revision": "efcb42da7c2e7d4eac1e",
+    "url": "/js/app.95864f8d.js"
+  },
+  {
+    "revision": "75bf8630377ab757f8ee",
+    "url": "/js/chunk-vendors.f7d56aed.js"
+  },
+  {
+    "revision": "b661c28b0f28606a96722ad2d9588b70",
+    "url": "/fonts/MaterialIcons-Regular.b661c28b.eot"
+  },
+  {
+    "revision": "9219a80f0478e0bfdee5f4c753ce8535",
+    "url": "/fonts/MaterialIcons-Regular.9219a80f.woff"
+  },
+  {
+    "revision": "586090b38a233ce0201fb221eb117a36",
+    "url": "/fonts/MaterialIcons-Regular.586090b3.ttf"
+  },
+  {
+    "revision": "8a04949228f4ee228253813b9530bd86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efcb42da7c2e7d4eac1e",
+    "url": "/css/app.25dd9034.css"
+  }
+];
